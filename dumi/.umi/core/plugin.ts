26c0d84b46cc1d56f30b182d61f95c27b9840ee7
wanlijun <wanlijun_cq@126.com>
2022-01-24 14:16:21 +0800

    feat:add less

--- dumi/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/home/lijunwan/caicai/project/vite/react-practice/node_modules/umi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
